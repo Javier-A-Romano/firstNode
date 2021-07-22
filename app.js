f01746effb3ae6f61c61234973e7582b3e1d1ba1
JavierR <romanojavier92@gmail.com>
2021-07-21 21:23:32 -0300

    First Node

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.get('/shorlak', (req, res) => {
+    res.send('shorlak op gg')
+})
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
