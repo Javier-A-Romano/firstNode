e44444911f670a96c98a6655a192123f408bd612
JavierR <romanojavier92@gmail.com>
2021-07-21 22:05:24 -0300

    public index

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
 app.get('/shorlak', (req, res) => {
     res.send('shorlak op gg')
-})
+});
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
